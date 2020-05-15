8e5e768625e12d8e78c76a378e06870244e3812a
Freddy Mota <freddyfx231@gmail.com>
2020-05-15 13:39:58 -0400

    Updates

---
+++ precache-manifest.46961976f9ae87621b64637d4fb03043.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "051d0df5bc6d673c09fce2aff65563f1",
+    "url": "/showdown-usage-stats-viewer/index.html"
+  },
+  {
+    "revision": "51e66dae8cf6617dd72a",
+    "url": "/showdown-usage-stats-viewer/static/css/main.179ba4b8.chunk.css"
+  },
+  {
+    "revision": "a4550c095d94ad5b45ed",
+    "url": "/showdown-usage-stats-viewer/static/js/2.2b26d311.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/showdown-usage-stats-viewer/static/js/2.2b26d311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51e66dae8cf6617dd72a",
+    "url": "/showdown-usage-stats-viewer/static/js/main.401802e0.chunk.js"
+  },
+  {
+    "revision": "c2eccabb056eda4723c3",
+    "url": "/showdown-usage-stats-viewer/static/js/runtime-main.a1409dbc.js"
+  }
+]);
